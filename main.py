4f96f7cdd73def17778a600dcf8b4997aa7ba84b
Phecobaba <mrpheco@gmail.com>
2023-08-27 21:43:04 +0100

    git ignore

---
+++ main.py
@@ -0,0 +1,8 @@
+input_1 = input ('enter sum of numbers ')
+input_2 = input('enter difference of numbers ' )
+
+first_number = (int(input_1) + int(input_2)) / 2
+second_number = first_number - int(input_2)
+
+print(f'The first number is: {int(first_number)}')
+print(f'The second number is: {int(second_number)}')
